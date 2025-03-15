89f663437b1050c7d52214d8ea05e5ddc50253f5
SmokeysScripts <Ianmarkealli14@gmail.com>
2025-03-15 11:17:26 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,13 +7,3 @@ AddEventHandler("revivePlayer", function()
     ClearPedLastWeaponDamage(playerPed)
     print("You have been revived!")
 end)
-
-RegisterNetEvent("respawnPlayer")
-AddEventHandler("respawnPlayer", function()
-    local playerPed = PlayerPedId()
-    local coords = vector3(307.87, -584.54, 43.28) -- Default respawn at a hospital
-    NetworkResurrectLocalPlayer(coords.x, coords.y, coords.z, 0.0, true, false)
-    SetEntityHealth(playerPed, 200)
-    ClearPedBloodDamage(playerPed)
-    print("You have been respawned!")
-end)
